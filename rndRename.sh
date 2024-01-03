0b6e67a62947e3f1f4e407ce774c0a0b8a3b09f3
Lars Janssen <lars.janssen@haw-hamburg.de>
2024-01-03 13:32:08 +0100

    Working on WIP

--- rndRename.sh
+++ rndRename.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+echo "Creating temporary folder and initializing variabels and paths"
 cd $1
 sourceFolder=$(pwd)
 cd ..
@@ -7,16 +8,31 @@ cd tempDestinationForRNDRename
 destFolder=$(pwd)
 cd $sourceFolder
 
-fileCount=$(ls $sourceFolder -l | grep -v ^d | wc -l)
 
+fileCount=$(ls $sourceFolder -l | grep -v ^d | wc -l)
+fileCountSize=${#fileCount}
+
+echo "Starting move and rename to temporary folder"
 for (( i = 1 ; i < $fileCount ; i++ )); do 
-fileToMove=$(ls $sourceFolder |sort -R | tail -1)
-extension="${fileToMove##*.}"
-echo $fileToMove
-echo $extension
-mv $sourceFolder/$fileToMove $destFolder/$i.$extension
+
+	newFNBeforeEx=$i
+	newFNSize=${#newFNBeforeEx}
+	zeroPadDiff=$(($fileCountSize-$newFNSize))
+
+	for ((n=0;n<$zeroPadDiff;n++)); do
+		newFNBeforeEx="0${newFNBeforeEx}"
+	done
+
+	fileToMove=$(ls $sourceFolder |sort -R | tail -1)
+	extension="${fileToMove##*.}"
+
+	mv $sourceFolder/$fileToMove $destFolder/$newFNBeforeEx.$extension
 done
 
+echo "Moveing files back from temporary folder into source folder"
 mv $destFolder/* $sourceFolder/
 
+echo "Deleting temporary Folder"
 rm -rf $destFolder
+
+echo "All "$fileCount"files renamed!"
