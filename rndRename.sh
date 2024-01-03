364b3ad0f548cec95cf625a7b3987bf3bc6acd60
Lars Janssen <lars.janssen@haw-hamburg.de>
2024-01-03 12:24:34 +0100

    tagging release w.t.f.

--- rndRename.sh
+++ rndRename.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/bash
-sourceFolder=$1
+cd $1
+sourceFolder=$(pwd)
+cd ..
+mkdir tempDestinationForRNDRename
+cd tempDestinationForRNDRename
+destFolder=$(pwd)
+cd $sourceFolder
+
 fileCount=$(ls $sourceFolder -l | grep -v ^d | wc -l)
 
 for (( i = 1 ; i < $fileCount ; i++ )); do 
-
 fileToMove=$(ls $sourceFolder |sort -R | tail -1)
-echo $fileToMove
-
+mv $sourceFolder/$fileToMove $destFolder/$i
 done
+
+mv $destFolder/* $sourceFolder/
+
+rm -rf $destFolder
